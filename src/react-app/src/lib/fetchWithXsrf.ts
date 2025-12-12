e633b1790e2f789b2eb310cfe98bcca2a4d70d39
coreybehrends <cbehrends@gmail.com>
2025-12-12 08:35:23 -0600

    chore(header): remove deprecated components/Header shim; header lives under features/header

--- src/react-app/src/lib/fetchWithXsrf.ts
+++
@@ -1,3 +0,0 @@
-// Re-export from new feature location for backwards compatibility
-// TODO: Update all imports to use '../features/authentication' directly and remove this file
-export { fetchWithXsrf } from "../features/authentication";
